9167a31315c1f87213f62c69dea346979a278d8a
Soundarya <soundaryakalal31@gmai.com>
2022-09-08 06:07:40 -0700

    ASP.NET DAY2

---
+++ WebAppDETAug2022/Pages/HeloPage.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppDETAug2022.Service;
+
+namespace WebAppDETAug2022.Pages
+{
+    public class HeloPageModel : PageModel
+    {
+      
+            public IHelo ob { get; set; }
+
+            public string Message { get; set; }
+
+            public HeloPageModel(IHelo i)
+            {
+                ob = i;
+            }
+            public void OnGet()
+            {
+                //ob = new Helo2();
+                Message = ob.SayHelo("sonu");
+            }
+     
+    }
+}
