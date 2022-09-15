0177beacad4d7b15ac629c49409ad47802659aae
Soundarya <soundaryakalal31@gmai.com>
2022-09-15 01:34:42 -0700

    UNIT TEST

---
+++ Library.API/Service/IBookService.cs
@@ -0,0 +1,16 @@
+﻿using Library.API.Models;
+
+namespace Library.API.Service
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+        Book Add(Book newBook);
+
+        Book GetById(Guid id);
+
+        void Remove(Guid id);
+    }
+}
+
+
